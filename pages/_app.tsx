eef97bc73d86aa2e6e412083a9d92d52f8790aa9
Stefano Andriolo <dev@srand.it>
2023-02-20 00:17:43 +0100

    feat: complete initial configuration
    Add the main tag in `_app.js` wrapping child components.
    
    Update global styles.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/theme/global'
+import { css } from '@/theme/stitches.config'
 import type { AppProps } from 'next/app'
 
+// style the main element in the HTML hierarchy with padding
+// and background color
+export const mainTagStyle = css({
+	height: '100%',
+	backgroundColor: '$grayDark2',
+	padding: '$small',
+	'@lg': {
+		padding: '$medium',
+	},
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	globalStyles()
+
+	return (
+		<main className={mainTagStyle()}>
+			<Component {...pageProps} />
+		</main>
+	)
 }
--- theme/global.ts
+++ theme/global.ts
@@ -1,64 +1,66 @@
-import { globalCss } from "./stitches.config"
+import { globalCss } from './stitches.config'
 
-export const globalStyles = globalCss({
-  /*
+const reset = {
+	/*
     1. Use a more-intuitive box-sizing model.
   */
-  '*, *::before, *::after': {
-    boxSizing: "border-box"
-  },
+	'*, *::before, *::after': {
+		boxSizing: 'border-box',
+	},
 
-  /*
+	/*
     2. Remove default margin
   */
-  '*': {
-    margin: 0
-  },
+	'*': {
+		margin: 0,
+	},
 
-  /*
+	/*
     3. Allow percentage-based heights in the application
   */
-  'html, body': {
-    height: '100%'
-  },
+	'html, body': {
+		height: '100%',
+	},
 
-  /*
+	/*
     Typographic tweaks!
     4. Add accessible line-height
     5. Improve text rendering
   */
-  'body': {
-    lineHeight: 1.5,
-    '-webkit-font-smoothing': 'antialiased'
-  },
+	body: {
+		lineHeight: 1.5,
+		'-webkit-font-smoothing': 'antialiased',
+	},
 
-  /*
+	/*
     6. Improve media defaults
   */
-  'img, picture, video, canvas, svg': {
-    display: 'block',
-    maxWidth: '100%'
-  },
+	'img, picture, video, canvas, svg': {
+		display: 'block',
+		maxWidth: '100%',
+	},
 
-  /*
+	/*
     7. Remove built-in form typography styles
   */
-  'input, button, textarea, select': {
-    font: 'inherit'
-  },
+	'input, button, textarea, select': {
+		font: 'inherit',
+	},
 
-
-  /*
+	/*
     8. Avoid text overflows
   */
-  'p, h1, h2, h3, h4, h5, h6': {
-    overflowWrap: "break-word"
-  },
+	'p, h1, h2, h3, h4, h5, h6': {
+		overflowWrap: 'break-word',
+	},
 
-  /*
+	/*
     9. Create a root stacking context
   */
-  '#root, #__next': {
-    isolation: "isolate"
-  }
-})
+	'#root, #__next': {
+		isolation: 'isolate',
+		height: '100%',
+	},
+}
+
+export const globalStyles = globalCss(reset)
