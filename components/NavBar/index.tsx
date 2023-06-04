df23a1fdc14e1cae121fca2d9641a42b412e6d4c
Stefano Andriolo <dev@srand.it>
2023-06-04 16:46:41 +0200

    feat: start making the Navbar interactive [#4]

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -9,10 +9,15 @@ const NavBar = ({ items }: NavbarModel) => {
 	const [activeItem, setActiveItem] = useState<NavbarItemModel>()
 	// item which is the only clickable element besides the hamburger button
 	const [nextItem, setNextItem] = useState<NavbarItemModel>()
+	const [open, setOpen] = useState<boolean>(false)
 	const router = useRouter()
 
 	if (!items?.length) {
 		throw new Error('No items provided')
+	}
+
+	const toggleMenuOpen = () => {
+		setOpen(!open)
 	}
 
 	useEffect(() => {
@@ -35,7 +40,7 @@ const NavBar = ({ items }: NavbarModel) => {
 				{activeItem?.label}
 			</Button>
 			<Button href={nextItem?.href}>{nextItem?.label}</Button>
-			<Button iconName="hamburger" onClick={(e) => console.debug({ e })} />
+			<Button iconName="hamburger" onClick={toggleMenuOpen} />
 		</StyledNavbar>
 	)
 }
