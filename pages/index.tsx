235dd10211e949714b4e109805b0cc8a9ae7f9bb
Stefano Andriolo <dev@srand.it>
2023-09-25 11:15:01 +0200

    feat: add Splide and configure it [#3]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,37 @@
-import PageSection from '@/components/Section'
-import SnappableContainer from '@/components/SnappableContainer'
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css'
 
 export default function Home() {
 	return (
-		<SnappableContainer>
-			<PageSection>1</PageSection>
-			<PageSection>2</PageSection>
-			<PageSection>3</PageSection>
-			<PageSection>4</PageSection>
-		</SnappableContainer>
+		<Splide
+			hasTrack={false}
+			options={{
+				speed: 1000,
+				autoWidth: true,
+				height: '100svb',
+				perPage: 1,
+				gap: 0,
+				pagination: false,
+				arrows: false,
+				drag: true,
+				snap: true,
+				dragMinThreshold: 10,
+				flickPower: 600,
+				flickMaxPages: 1,
+				waitForTransition: true,
+				preloadPages: 1,
+				wheel: true,
+				keyboard: true,
+				direction: 'ttb',
+			}}
+		>
+			<SplideTrack>
+				<SplideSlide>Slide 1</SplideSlide>
+				<SplideSlide>Slide 2</SplideSlide>
+				<SplideSlide>Slide 3</SplideSlide>
+				<SplideSlide>Slide 4</SplideSlide>
+				<SplideSlide>Slide 5</SplideSlide>
+			</SplideTrack>
+		</Splide>
 	)
 }
